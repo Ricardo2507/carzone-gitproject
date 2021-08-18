6ca7f97815fa8b92806e571d9f349c65515b3ec6
Ricardo Sérgio <rscarvalho@hotmail.com>
2021-08-18 12:54:16 -0300

    Terminando modelo de Team

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'C@rol2907',
+        'HOST': 'localhost',
+        
     }
 }
 
@@ -123,6 +127,11 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'carzone/static'
 ]
+
+# para usarmos Media(áudios, vídeos, imagens)
+
+MEDIA_ROOT = BASE_DIR / 'media' 
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
